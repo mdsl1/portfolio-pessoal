902d89e9b655372c5ad2a2ac776b2504a318dd4e
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2025-06-23 10:29:42 -0300

    Fazendo alterações no estilo do index

--- js/Script.js
+++ js/Script.js
@@ -157,6 +157,20 @@ function createCards(dados) {
     });
 }
 
+window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    const conteudo = document.getElementById("mainContent");
+
+    // Adiciona classe de fade-out
+    preloader.classList.add("fade-out");
+
+    // Aguarda a transição para esconder de vez
+    setTimeout(() => {
+      preloader.style.display = "none";
+      conteudo.style.display = "block";
+    }, 1000); // corresponde à duração da transição
+  });
+
 // Chama a função de importar o JSON ao carregar a página
 document.addEventListener('DOMContentLoaded', carregarDadosJson);
 // Chama a função para ativar os popovers ao carregar a página
